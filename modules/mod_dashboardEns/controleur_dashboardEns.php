1075e6626828ffaec267c20ef66904fe3c5b7bf0
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-01-05 13:55:08 +0100

    nettoyage

--- modules/mod_dashboardEns/controleur_dashboardEns.php
+++
@@ -1,6 +0,0 @@
-<?php
-class ControleurDashboardEns {
-    public function exec() {
-        render_template("<h2>Bienvenue sur le tableau de bord Enseignant</h2><p>Gérez les projets des étudiants et les rendus.</p>");
-    }
-}
--- modules/mod_dashboardEtu/controleur_dashboardEtu.php
+++
@@ -1,6 +0,0 @@
-<?php
-class ControleurDashboardEtu {
-    public function exec() {
-        render_template("<h2>Bienvenue sur le tableau de bord Étudiant</h2><p>Accédez à vos cours, SAE, et soutenances.</p>");
-    }
-}
