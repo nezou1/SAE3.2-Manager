0211eeec51e2b3229f2cc107a0383e3705afb555
Lucas Delacroix <lucas.delacroix6@gmail.com>
2025-01-08 17:23:23 +0100

    ajout du logo dans le nav + suppression du templateConnexion

--- core/site.php
+++ core/site.php
@@ -15,42 +15,33 @@ class Site {
 
 		switch($this->menuNom) {
 			case "enseignant":
-				require_once PROJECT_ROOT . "/composants/menu/enseignants/composant_menu_enseignant.php";
-				break;
 			case "etudiant":
-				require_once PROJECT_ROOT . "/composants/menu/etudiants/composant_menu_etudiant.php";
+				require_once PROJECT_ROOT . "composants/menu/".$this->menuNom."s/composant_menu_".$this->menuNom.".php";
 				break;
 			case "connexion":
-				require_once "../composants/menu/connexion/composant_menu_connexion.php";
+				require_once PROJECT_ROOT . "composants/menu/".$this->menuNom."/composant_menu_".$this->menuNom.".php";
+				break;
+			default :
+				die ("Menu inexistant");
+				break;
+			
 		}
 		$menu_class = "ComposantMenu".ucfirst($this->menuNom);
 		$this->menu = new $menu_class();
 
-	
-
-
 		switch ($this->moduleNom) {
-		
 			case "inscription" :  
-				require_once "../modules/mod_".$this->moduleNom."/module_".$this->moduleNom.".php";
-				break;
 			case "connexion" :
-				require_once "../modules/mod_".$this->moduleNom."/module_".$this->moduleNom.".php";
-				break;
 			case "mdpOublie" :
-				require_once "../modules/mod_".$this->moduleNom."/module_".$this->moduleNom.".php";
-				break;
 			case "dashboard" :	
-				require_once "../modules/mod_".$this->moduleNom."/module_".$this->moduleNom.".php";
-				break;
 			case "sae":
-			case "soutenance" :                
+			case "soutenance" :
+			case "depot":                
 				require_once PROJECT_ROOT . "/modules/mod_".$this->moduleNom."/module_".$this->moduleNom.".php";
 				break;
 			default :
 				die ("Module inexistant");
 		}
-
 	}
 	
 	
--- templates/template.php
+++ templates/template.php
@@ -15,16 +15,64 @@
         <link rel="stylesheet" href="../assets/css/form.css">
         <link rel="stylesheet" href="../assets/css/footer.css">
     </head>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #F7F1E9;
+            min-height: 100vh;
+        }
+        .container {
+            max-width: 600px;
+        }
+        .navbar {
+            background-color: #91A89B;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        .navbar .nav-link, .navbar .navbar-brand {
+            color: white;
+            border-radius: 5px;
+            padding-left: 5px;
+        }
+
+        .navbar .nav-link:hover, .navbar .navbar-brand:hover {
+            background-color: #778c7b;
+            transition-duration: 0.4s;
+            border-radius: 5px;
+        }
+        .logo {
+            width: 150px;
+            margin: 0 auto 20px;
+        }
+        button {
+            background-color: #6c757d;
+            color: white;
+        }
+        button:hover {
+            background-color: #5a6268;
+        }
+
+        .btn {
+            background-color: #E6E6E6;
+            border: solid 1px #767676;
+        }
+
+        main {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
     <body>
         <header>
             <nav class="navbar navbar-expand-lg">
                 <?php echo $menu->getAffichage();?>
             </nav>
         </header>
-        <main>
-            <section>
-                <?=$module_html?>
-            </section>
+        <main class="main">
+            <?=$module_html?>
         </main>
         <footer>
             <?php echo $footer->getAffichage();?>
